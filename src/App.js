a2725c6ba0b4fe7867b2cce38fcb131b9fb668b3
Christopher Hannelas <101250058+christopherhannelas@users.noreply.github.com>
2022-06-20 14:26:29 +0400

    adding loading spinner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import { Oval } from 'react-loader-spinner';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ScrollToTop from './components/autoscroll/autoscroll.component';
@@ -7,13 +8,32 @@ import Background from './components/background/background.component';
 import AnimatedRoutes from './routes/animated-routes.jsx';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
+  });
   return (
     <div className='App'>
       <Background />
 
       <Router>
         <ScrollToTop />
-        <AnimatedRoutes />
+        {isLoading ? (
+          <div className='test'>
+            <Oval
+              color='#2d8e67'
+              secondaryColor='white'
+              strokeWidth={5}
+              strokeWidthSecondary={4}
+              height={100}
+              width={100}
+            />
+          </div>
+        ) : (
+          <AnimatedRoutes />
+        )}
       </Router>
     </div>
   );
